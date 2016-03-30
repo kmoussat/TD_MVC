936120975683e7f6d39a2c5087340f3aab7b16a1
Ken Moussat <ken.moussat@devinci.fr>
2016-03-30 16:57:15 +0200

    wpf

---
+++ Wpf2/Wpf2/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf2
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //gauche à droite
+            if (txt1.Text == "") MessageBox.Show("Il n'y a rien d'écrit");
+            string msg = txt1.Text;
+            txt2.Text = msg;
+            txt1.Clear();
+
+         
+
+
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+         
+            //droite à gauche
+            if (txt2.Text == "") MessageBox.Show("Il n'y a rien d'écrit");
+            string msg = txt2.Text;
+            txt1.Text = msg;
+            txt2.Clear();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ wpf3_alpha/wpf3/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf3
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                Random r = new Random();
+                double numb = r.NextDouble();
+                numb = numb * (canvas1.ActualHeight - Rect.Height);// - Rect.ActualHeight / 2;
+
+            /*if (numb<0)
+            {
+                numb = 0;
+            }
+            */
+                Thread.Sleep(5);
+                double longueur;
+                Random r1 = new Random();
+                longueur = r1.NextDouble();
+                longueur = longueur * (canvas1.ActualWidth - Rect.Width);// - Rect.ActualHeight / 2;
+
+            /*
+
+            if (longueur<0)
+            {
+                longueur = 0;
+            }
+            */
+                Rect.Margin = new Thickness(longueur, numb, 0, 0);
+            
+
+
+        }
+
+       
+    }
+}
